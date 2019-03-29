72813b4591bba252a742de930e419c5fe9a683d8
Steven Paulino <stevenpaulino1@hotmail.com>
2019-03-28 23:48:00 -0400

    working mobile responsive

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 import classes from "./NavigationItem.css";
-
+// keep making mobile responsive
 const navigationItem = props => (
   <li className={classes.NavigationItem}>
     <a href={props.link} className={props.active ? classes.active : null}>
