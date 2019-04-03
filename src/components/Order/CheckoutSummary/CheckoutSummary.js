14275f8f931f90a0f8c2cfa0f413ac4c7bad5f12
Steven Paulino <stevenpaulino1@hotmail.com>
2019-04-02 23:56:56 -0400

    Finished checkoutsummary component

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const checkoutSummary = props => {
+  return (
+    <div>
+      <h1>Hope you enjoy the burger!</h1>
+    </div>
+  );
+};
+
+export default checkoutSummary;
