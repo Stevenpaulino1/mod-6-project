297767a9e612a42684e6a12e73bb58d9bc170e52
Steven Paulino <stevenpaulino1@hotmail.com>
2019-04-16 17:50:40 -0400

    Orders container and componenet created

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Order from "../../components/Order/Order";
+import classes from "./Order.css";
+class Orders extends Component {
+  render() {
+    return (
+      <div>
+        <Order />
+      </div>
+    );
+  }
+}
+export default Orders;
