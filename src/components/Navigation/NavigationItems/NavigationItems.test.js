84eb328e2ed227564a87feaf6e4a0456f4836659
Steven Paulino <stevenpaulino1@hotmail.com>
2019-08-21 00:00:13 -0400

    tests

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -21,4 +21,10 @@ describe("<NavigationItems/>", () => {
     wrapper.setProps({ isAuthenticated: true });
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
+  it("should render three nav elements if authenticated", () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(
+      wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)
+    ).toEqual(true);
+  });
 });
